547747f2323b579e135c66c00dfc8302da2a6524
fwcd <fwcdmail@gmail.com>
2020-06-04 20:15:35 +0200

    Add Webpack bundling

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+// Based on https://code.visualstudio.com/api/working-with-extensions/bundling-extension
+
+module.exports = {
+    target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: {
+        path: path.resolve(__dirname, "out"), // the bundle is stored in the 'out' folder (see the package.json), 📖 -> https://webpack.js.org/configuration/output/
+        filename: "extension.js",
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]"
+    },
+    devtool: "source-map",
+    externals: {
+        vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: {
+        extensions: [".ts", ".js"] // support TypeScript and JavaScript sources, 📖 -> https://github.com/TypeStrong/ts-loader
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            }
+        ]
+    }
+};
